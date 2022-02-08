e1cfa9b05c570278d70ea64e0b425015f55fa9e6
JostesMarcel <jostes77@googlemail.com>
2022-02-08 16:10:18 +0100

    JS2.2_DOM_Elements

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let vorname = document.getElementById("vorname");
+let nachname = document.getElementById("nachname");
+let land = document.getElementById("land");
+
+// console.log(vorname)
+// console.log(nachname)
+// console.log(land)
+
+function data() {
+    console.log("Full name: " + vorname.value +  " " + nachname.value + " " + land.value)
+}
